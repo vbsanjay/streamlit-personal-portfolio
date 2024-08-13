6a1213a8e0f720061feb2f7caf8327ce5ad82994
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-08-13 22:20:07 +0530

    experience section updated

---
+++ experience_section.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def experience_section():
+    st.write("#")
+    st.subheader("Other Experience")
+    st.markdown("**Amazon** &mdash; ***Senior Testing Associate***")
+    st.markdown("*Oct. 2019 — Sep. 2022*")
+    st.write("""
+    - Played a key role in the Awil IoT project, improving Wi-Fi and Bluetooth connectivity for Amazon Echo devices.
+    - Identified critical bugs in automation scripts and collaborated with peers to fix them, increasing the pass rate from 60% to 92%.
+    """)
