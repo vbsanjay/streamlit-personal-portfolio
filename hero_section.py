0dfa95ff168286ebe01fe1dfb16c7f1a670e2812
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-08-13 22:17:44 +0530

    about me and picture displayed

---
+++ hero_section.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+def hero_section(profile_pic, pdf_byte):
+    col1, col2 = st.columns(2, gap="small")
+    with col1:
+        st.image(profile_pic, width=230)
+    with col2:
+        st.title("👋🏼 Hello! there, It's Sanjay V B")
+        st.write("""
+        I’m a software developer with expertise in Python, Django, and SQL, 
+        and a solid foundation in data structures and algorithms. I bring a 
+        blend of theoretical knowledge and practical project experience.
+        """)
+        st.download_button(
+            label=" 📝 Download Resume",
+            data=pdf_byte,
+            file_name="CV.pdf",
+            mime="application/octet-stream"
+        )
+        st.write("📧 vbsanjay815@gmail.com")
