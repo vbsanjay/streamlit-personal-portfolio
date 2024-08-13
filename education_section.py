83d9e08830120ceb1838ba493a970a150878e98c
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-08-13 22:19:55 +0530

    education section updated

---
+++ education_section.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+def education_section():
+    st.write("#")
+    st.subheader("Education")
+    st.markdown("""
+    ### **Software Development**
+    **Scaler Academy**  
+    *Sep. 2022 — Aug. 2024*
+
+    ### **Bachelor of Engineering - Civil**
+    **S.A Engineering College**  
+    *Aug. 2014 — May 2018*
+
+    ### **XII - Computer Science**
+    **Sethu Bhaskara School**  
+    *Mar. 2014*
+    """)
