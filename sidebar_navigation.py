ef7f12f5e23af243fa1ce2dc4bd9ca9d200b2385
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-08-14 00:06:47 +0530

    bug fix: side bar

--- sidebar_navigation.py
+++ sidebar_navigation.py
@@ -2,6 +2,12 @@ import streamlit as st
 
 def sidebar_navigation():
     st.sidebar.title("Sanjay's Portfolio")
-    subheadings = ["Connect-with-me", "Skills", "Academic-Projects"]
+    subheadings = {"Connect with me":"connect-with-me", 
+                   "Skills": "skills", 
+                   "Academic Projects": "academic-projects",
+                   "Other Experience": "other-experience",
+                   "Achievements": "achievements",
+                   "Education": "education"
+                   }
     for subheading in subheadings:
-        st.sidebar.markdown(f"[{subheading}](#{subheading.lower()})")
+        st.sidebar.markdown(f"[{subheading}](#{subheadings[subheading]})")
