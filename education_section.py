0ad37a8d2549b06039ec3dd9657e464ab4dfe62c
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-08-13 23:47:06 +0530

    emonjies added

--- education_section.py
+++ education_section.py
@@ -4,15 +4,15 @@ def education_section():
     st.write("#")
     st.subheader("Education")
     st.markdown("""
-    ### **Software Development**
+    ### 💻 **Software Development**
     **Scaler Academy**  
     *Sep. 2022 — Aug. 2024*
 
-    ### **Bachelor of Engineering - Civil**
+    ### 👨🏼‍🎓 **Bachelor of Engineering - Civil**
     **S.A Engineering College**  
     *Aug. 2014 — May 2018*
 
-    ### **XII - Computer Science**
+    ### 📝 **XII - Computer Science**
     **Sethu Bhaskara School**  
     *Mar. 2014*
     """)
