4f58bfb6ec54d2a08c93bef3223ba1065fdd623e
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-08-13 22:19:43 +0530

    achievements were updated

---
+++ achievements_section.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+def achievements_section():
+    st.write("#")
+    st.subheader("Achievements")
+    st.markdown("**Scaler Academy**")
+    st.write("""
+    - Top 10 in the DSA Contest.
+    - 400 days coding streak.
+    """)
+    st.markdown("**Amazon**")
+    st.write("""
+    - Promoted from Testing Associate to Senior Testing Associate
+    """)
