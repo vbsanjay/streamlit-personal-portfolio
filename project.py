a83713888993e062b3129f21c9de0d0b4af25c5e
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-08-13 22:18:42 +0530

    project fetched from github api

---
+++ project.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import requests
+
+# function to fetch project from my git hub
+def projects():
+    GITHUB_USERNAME = 'vbsanjay'
+    project_list = ['Python-Django-DevSearch']
+    url = f'https://api.github.com/users/{GITHUB_USERNAME}/repos'
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        repos = response.json()
+        for repo in repos:
+            if repo['name'] in project_list:
+                topic_string = ""
+                with st.expander(f"🔥 {repo['name']}"):
+                    st.link_button("Check code here",repo['html_url'])
+                    st.write(repo["description"])
+                    for topic in repo['topics']:
+                        topic_string += f"#{topic}  "
+                    st.info(topic_string)
+    else:
+        print(f"Failed to retrieve repositories: {response.status_code}")
