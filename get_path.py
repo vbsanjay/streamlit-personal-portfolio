2bb7de2408df763d8004dca8ed397ef7a06d1852
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-08-14 08:16:33 +0530

    resume file updated

--- get_path.py
+++ get_path.py
@@ -6,6 +6,6 @@ def get_paths():
     current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
     return {
         "css_file": current_dir / "styles" / "main.css",
-        "resume_file": current_dir / "assets" / "CV.pdf",
+        "resume_file": current_dir / "assets" / "Sanjay_Resume_Software_Developer.pdf",
         "profile_pic": current_dir / "assets" / "profile-pic.png"
     }
